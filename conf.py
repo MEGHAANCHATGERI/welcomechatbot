204938bc783d0ea3b8eb2077c15b10312189edde
ANCHATGERIMEGHA <megha.anchatgeri@redeemsystems.com>
2019-02-28 15:57:54 +0530

    chnge

--- conf.py
+++ conf.py
@@ -9,8 +9,7 @@
 # -- Path setup --------------------------------------------------------------
 
 # If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
+# add these directories to sys.path here. If the directory is relative to the documentation root, use os.path.abspath to make it absolute, like shown here.
 #
 import os
 import sys
@@ -24,8 +23,6 @@ import sphinx_rtd_theme
 sys.path.insert(0, os.path.abspath('.'))
 # sys.path.append("audio.rst")
 # sys.path.append("coclusion.rst")
-# -- Project information -----------------------------------------------------
-
 project = 'Megha'
 copyright = '2019, chaithra'
 author = 'chaithra'
